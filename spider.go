639bac9bd582d95c5a8ccd97c12571c1bd10ae5d
ccQpein <ccqpein@protonmail.com>
2017-04-28 11:56:28 -0400

    finish major functions

--- spider.go
+++ spider.go
@@ -4,70 +4,62 @@ import (
 	. "fmt"
 	. "github.com/PuerkitoBio/goquery"
 	"log"
-	"strconv"
-	. "strings"
+	//	"strconv"
+	//	. "strings"
 )
 
 var BaseUrls = []string{
 	"https://www.indeed.com/jobs?q=%[1]s&l=%[2]s",
 }
 
+var KeyWords = []string{}
+
+var Location = []string{}
+
 type TagStructures []string
 
-func GetSearchPages(baseUrls, keyWords []string) []*Document {
+func GetSearchPages(baseUrls, keyWords, location []string) []*Document {
 	var results []*Document
 	for _, url := range baseUrls {
-		doc, err := NewDocument(Sprintf(url, keyWords[0], keyWords[1]))
-		if err != nil {
-			log.Fatal(err)
+		for _, l := range location {
+			for _, w := range keyWords {
+				doc, err := NewDocument(Sprintf(url, w, l))
+				if err != nil {
+					log.Fatal(err)
+				}
+				results = append(results, doc)
+			}
 		}
-		results = append(results, doc)
 	}
 	return results
 }
 
 func GetAllNodes(s *Document, tagStructures TagStructures) *Selection {
-	var temp *Selection
-	for _, v := range tagStructures {
-		temp = s.Find(v)
+	temp := s.Find(tagStructures[0])
+	for _, v := range tagStructures[1:] {
+		temp = temp.Find(v)
 	}
 	return temp
 }
 
-/*
-func FilterSelectionsDate(s *Selection, days int) {
-
-	s.FilterFunction(func(_ int, s *Selection) bool {
-		if day, _ := strconv.Atoi(Split(s.Find(".date").Text(), " ")[0]); day <= days {
-			return true
-		} else {
-			return false
-		}
-	}).Each(func(_ int, s *Selection) {
-		title := s.Find(".jobtitle").Text()
-		link, ok := s.Find(".jobtitle").Find("a").Attr("href")
-		Println(title)
-		Println(ok, link)
-	})
-}*/
-
 func GetTitleAndLink(s *Selection) {
-
 	s.Each(func(_ int, s *Selection) {
 		title := s.Find(".jobtitle").Text()
-		link, ok := s.Find(".jobtitle").Find("a").Attr("href")
-		Println(title)
-		Println(ok, link)
+		if link, ok := s.Find(".jobtitle").Find("a").Attr("href"); ok {
+			Println(title)
+			Println("https://www.indeed.com" + link)
+		}
 	})
 }
 
 func main() {
-	testkeyW := []string{"haskell", ""}
+	testkeyW := []string{"haskell", "python", "golang", "lisp"}
 	testtagStrc := []string{".row.result"}
+	testLocation := []string{"Washington%2C+DC", "Chicago%2C+IL", "Boston%2C+MA"}
 
-	a := GetSearchPages(BaseUrls, testkeyW)
+	a := GetSearchPages(BaseUrls, testkeyW, testLocation)
 
-	Println("haha")
-
-	FilterSelectionsDate(GetAllNodes(a[0], testtagStrc), 9)
+	for _, doc := range a {
+		GetTitleAndLink(GetAllNodes(doc, testtagStrc))
+	}
 }
